474f0c30da99bf6915d4c858ae5bf25ff0354944
Tinashe Muchie <tinashemuchineripi@gmail.com>
2022-06-07 00:30:07 +0400

    useQuery addition to query for blogs on graphcms

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,8 +1,17 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 import styles from "../styles/blog.module.css";
+import { useQuery, ApolloError } from "@apollo/client";
+import { BLOGS } from "../queries/blogsQuerie";
+import { MyQueryQuery } from "../src/generated/graphql";
 
-const BlogPage = () => (
+interface blogpageIF {
+  blogs: blogs_type;
+  loading: boolean;
+  error: ApolloError;
+}
+
+const BlogPage = ({blogs, loading, error }: blogpageIF) => (
   <Layout title="blog-page">
     <main className={styles.container}>
       <h2> Blog Page </h2>
@@ -17,3 +26,42 @@ const BlogPage = () => (
 );
 
 export default BlogPage;
+
+export async function getServerSideProps() {
+  const { loading, error, data } = await useQuery<MyQueryQuery>(BLOGS);
+
+  return {
+    props: {
+      blogs: data?.blogs,
+      loading,
+      error
+    },
+  };
+}
+
+type blogs_type = Array<{ 
+  __typename?: 'Blog', 
+  id: string, 
+  slug: string, 
+  title: string, 
+  dateAndTime: any, 
+  coverPhoto: { 
+    __typename?: 'Asset', 
+    url: string 
+  }, 
+  content: { 
+    __typename?: 'BlogContentRichText', 
+    html: string 
+  }, 
+  author?: { 
+    __typename?: 'Author', 
+    id: string, 
+    name: string, 
+    avatar: { 
+      __typename?: 'Asset', 
+      id: string, 
+      url: string }, 
+      blogs: Array<{ 
+        __typename?: 'Blog', 
+        title: string }> 
+      } | null }>
